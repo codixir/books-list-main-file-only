5f7a94498e556dc65b2137fb30e6b9e9a8535aee
codixir <codixir@gmail.com>
2018-09-01 07:25:05 -0400

    Create a router instance from gorilla/mux, register the api endpoints and their respective callback functiions on the router, listen to the application and serve it on port 8000, define the handlers callback functions

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	// "encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Book struct {
 	ID     string `json:id`
 	Title  string `json:title`
@@ -10,4 +19,33 @@ type Book struct {
 var books []Book
 
 func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Gets all the books")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Gets a single the book by its id")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Adds a book")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Updates a book")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Removes a book")
 }
